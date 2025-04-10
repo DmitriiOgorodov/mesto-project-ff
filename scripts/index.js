f3f0df52c2c90eb08f6f173f3522c7b757950c0d
dmitrii ogorodov <dmitrii.ogorodov@yandex.ru>
2025-04-10 23:36:19 +0500

    Автоматическое добавление карточек и реализация кнопки удалить

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,24 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content
+const places__list = document.querySelector('.places__list')
+
+initialCards.forEach((item) => {
+  const card = cardTemplate.querySelector('.places__item').cloneNode(true)
+  const cardDeleteButton = card.querySelector('.card__delete-button')
+
+  card.querySelector('.card__image').src = item.link;
+  card.querySelector('.card__title').textContent = item.name;
+
+  places__list.append(card)
+
+  removeCard(cardDeleteButton)
+})
+
+function removeCard (button) {
+  button.addEventListener('click', (evt) => {
+    evt.target.closest('.places__item').remove();
+  })
+}
 
 // @todo: DOM узлы
 
